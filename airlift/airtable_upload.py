8ce33cc7ec64468e40a97c57b05b83c3a647c542
Arjun <arjunprakash027@gmail.com>
2023-10-26 18:21:04 +0530

    typing hint chages

--- airlift/airtable_upload.py
+++ airlift/airtable_upload.py
@@ -1,5 +1,6 @@
 import pyairtable.api.table as ATtable
 from airlift.csv_data import CSVRowType
+from typing import Any, Dict, Iterable, Iterator, List, Optional
 
-def upload_data(client: ATtable, new_data:list[CSVRowType]) -> None:
+def upload_data(client: ATtable, new_data:List[CSVRowType]) -> None:
     client.batch_create(new_data)
